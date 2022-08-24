db5e915e697a87259e894be7b0de9e8c636021b9
Thiago Leite <thiagoleites@outlook.com.br>
2022-08-23 21:53:34 -0300

    Script com funções para obtenção dos dados

---
+++ app.js
@@ -0,0 +1,32 @@
+/*
+Descrição
+Implemente uma função que receba um número inteiro positivo e retorne o
+somatório de todos os valores inteiros divisíveis por 3 ou 5 que seja 
+inferires ao número passado como parâmetro.
+*/
+
+const numero = document.getElementById('numero');
+const btn = document.getElementById('btn');
+const msg = document.getElementById('mensagem');
+const opt = document.getElementById('opcoes');
+
+//Evento de click do botão onde será chamada a função handleButtonClick
+
+btn.addEventListener('click', handleButtonClick);
+
+//Função que será chamada pelo evento de click do botão
+function handleButtonClick() {
+    let num = Number(numero.value);
+    msg.textContent = getNumber(num);
+}
+
+//Função que retorna o somatório de todos os valores inteiros divisíveis por 3 ou 5 que seja inferiores ao número passado como parâmetro
+function getNumber(number){
+    let sum = 0;
+    for(let i = 0; i < number; i++){
+        if(i % 3 == 0 || i % 5 == 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
